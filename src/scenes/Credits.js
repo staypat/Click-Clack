48463a280eac8fb210e1e841306027aae6b9d578
Patrick Hu <phhu@ucsc.edu>
2023-12-14 22:59:25 -0800

    Updated credits

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -14,8 +14,8 @@ class Credits extends Phaser.Scene{
         this.menuBgm.play();
         cursors = this.input.keyboard.createCursorKeys();
         this.add.image(40, game.config.height - 40, 'button').setScale(1.5).setAngle(180);
-        this.add.bitmapText(25, game.config.height/2 - 100, 'klein', 'Programmed by Patrick Hu\nArt Assets by Patrick Hu from Aseprite and Pixilart\nBackground Music and SFX by soundsgreat and Pixabay\nFont by (Font: Klein Family by Zetafonts\n -http://www.zetafonts.com/collection/       )').setScale(0.37).setCenterAlign();
-        this.add.text(635, 310, '2922').setColor('black').setScale(1.5);
+        this.add.bitmapText(25, game.config.height/2 - 100, 'klein', 'Based on The Simpsons\nEpisode: \"Please Homer, Don\'t Hammer \'Em\"\nProgrammed by Patrick Hu\nArt Assets by Patrick Hu from Aseprite and Pixilart\nBackground Music and SFX by soundsgreat and Pixabay\nFont by (Font: Klein Family by Zetafonts\n -http://www.zetafonts.com/collection/       )').setScale(0.37).setCenterAlign();
+        this.add.text(635, 365, '2922').setColor('black').setScale(1.5);
         this.add.bitmapText(90, game.config.height - 50, 'klein', 'to go back to menu').setScale(0.4);
     }
     update(){
